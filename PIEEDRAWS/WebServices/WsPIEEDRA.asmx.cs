793b479baf8fc6ed8e0de4af8153ba4e98982791
DevVSProjects <antaluiv8@gmail.com>
2016-05-08 17:52:00 -0500

    Conexion a la BD para Ingresar al Sistema

--- PIEEDRAWS/WebServices/WsPIEEDRA.asmx.cs
+++ PIEEDRAWS/WebServices/WsPIEEDRA.asmx.cs
@@ -1,9 +1,12 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using System.Data.SqlClient;
+using Conexion;
 
 namespace PIEEDRAWS.WebServices
 {
@@ -19,9 +22,49 @@ namespace PIEEDRAWS.WebServices
     {
 
         [WebMethod]
-        public string HelloWorld()
+        public bool AutenticaUser(string usuario, string password, ref int _resultado, ref string _mensaje)
         {
-            return "Hello World";
+            DataSet result =  new DataSet();
+            SqlParameter[] parametros = new SqlParameter[4];
+
+            parametros[0] = SqlHelper.CreateParameter("@Usuario", usuario, SqlDbType.VarChar, ParameterDirection.Input,
+                20);
+            parametros[1] = SqlHelper.CreateParameter("@Contraseña", password, SqlDbType.VarChar, ParameterDirection.Input,
+                20);
+            parametros[2] = SqlHelper.CreateParameter("@Resultado", SqlDbType.Int, ParameterDirection.Output,
+                8);
+            parametros[3] = SqlHelper.CreateParameter("@Mensaje", SqlDbType.VarChar, ParameterDirection.Output,
+                100);
+
+            SqlConnection con = new SqlConnection();
+            string strincon = System.Configuration.ConfigurationManager.ConnectionStrings["PIEEDRAConnectionString"].ConnectionString;
+            con.ConnectionString = strincon;
+            SqlCommand cmd = null;
+
+            try
+            {
+                if (con.State == System.Data.ConnectionState.Closed)
+                {
+                    result = SqlHelperPlus.ExecuteDatasetPlus(con, CommandType.StoredProcedure, "ValidaUsuario",
+                        parametros);
+                    _resultado = Convert.ToInt32(parametros[2].Value);
+                    _mensaje = parametros[3].Value.ToString();
+
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            if (_resultado != 1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
